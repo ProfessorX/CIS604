41e6afd6e43ff884d58574941001c526dde0cc48
Abraham Xiao <q289196573@gmail.com>
2014-11-08 23:56:58 +0400

    Update notes and sample codes. Homework solutions working in progress. They will be finished the moment UCC501 midterm terminates.

--- Notes/Midterm/auto/Review-Notes.el
+++ Notes/Midterm/auto/Review-Notes.el
@@ -1,18 +1,25 @@
-(TeX-add-style-hook "Review-Notes"
+(TeX-add-style-hook
+ "Review-Notes"
  (lambda ()
-    (LaTeX-add-labels
-     "sec:artif-intell"
-     "sec:problem-solving")
-    (TeX-run-style-hooks
-     "graphicx"
-     "xcolor"
-     "geometry"
-     "margin=10mm"
-     "amssymb"
-     "amsmath"
-     ""
-     "latex2e"
-     "art10"
-     "article"
-     "twocolumn")))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "twocolumn")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=10mm")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "amsmath"
+    "amssymb"
+    "geometry"
+    "xcolor"
+    "graphicx")
+   (LaTeX-add-labels
+    "sec:artif-intell"
+    "sec:problem-solving"
+    "sec:uninformed-search"
+    "sec:informed-search"
+    "eq:1"
+    "sec:constr-satisf-probl"
+    "sec:adversarial-search")))
 
